2a19511b5f134c46e6bdcb8de33139853354d974
dengjia <dj278016981@gmail.com>
2021-07-06 15:14:57 +0800

    add echarts

---
+++ src/components/report/Report.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+        <!-- 为ECharts准备一个具备大小（宽高）的Dom -->
+    <div id="main" style="width: 600px;height:400px;"></div>
+
+    </el-card>
+  </div>
+</template>
+
+<script>
+import * as echarts from 'echarts'
+// 1.安装依赖
+// 2. 引入依赖
+// 3.给echarts准备一个dom
+// 4. 等dom渲染完成后使用echarts绘制图标(mounted)
+// 5.将数据渲染到echarts里
+export default {
+  data() {
+    return {}
+  },
+  created() {},
+  //   dom渲染完成后才能加载echarts
+  mounted() {
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('main'))
+
+    // 指定图表的配置项和数据
+    var option = {
+      title: {
+        text: 'ECharts 入门示例'
+      },
+      tooltip: {},
+      legend: {
+        data: ['销量']
+      },
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+      },
+      yAxis: {},
+      series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }]
+    }
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option)
+  },
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped></style>
